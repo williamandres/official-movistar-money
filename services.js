fb2167da840695571b5cb553ec94f5fc588e60ea
William Aguilar <williamaguilar@misena.edu.co>
2021-10-04 16:21:58 -0500

    add interactivity to service-help-button-icon-container and help-button-icon

--- services.js
+++ services.js
@@ -38,25 +38,59 @@ function service2()
 
 /* ********************** services help section  ****************************** */
 
-const commercialInformation = document.getElementById('commercial-information')
-const myAccount = document.getElementById('my-account')
-const payments = document.getElementById('payments')
-const security = document.getElementById('security')
+const commercialInformation = document.getElementById('commercial-information');
+const myAccount = document.getElementById('my-account');
+const payments = document.getElementById('payments');
+const security = document.getElementById('security');
 
-const commercialQuestions = document.getElementById('commercial-innformation-questions')
-const accountQuestions = document.getElementById('my-account-questions')
-const paymentsQuestions = document.getElementById('payments-questions')
-const securityQuestions = document.getElementById('security-questions')
+const serviceButton1Gray = document.getElementById('service-button1-gray');
+const serviceButton1Blue = document.getElementById('service-button1-blue');
+const serviceButton2Gray = document.getElementById('service-button2-gray');
+const serviceButton2Blue = document.getElementById('service-button2-blue');
+const serviceButton3Gray = document.getElementById('service-button3-gray');
+const serviceButton3Blue = document.getElementById('service-button3-blue');
+const serviceButton4Gray = document.getElementById('service-button4-gray');
+const serviceButton4Blue = document.getElementById('service-button4-blue');
 
 
-commercialInformation.addEventListener('click', commercialIn)
+const commercialQuestions = document.getElementById('commercial-innformation-questions');
+const accountQuestions = document.getElementById('my-account-questions');
+const paymentsQuestions = document.getElementById('payments-questions');
+const securityQuestions = document.getElementById('security-questions');
+
+const serviceButtonName1 = document.getElementById('service-button-name1'); 
+const serviceButtonName2 = document.getElementById('service-button-name2');
+const serviceButtonName3 = document.getElementById('service-button-name3'); 
+const serviceButtonName4 = document.getElementById('service-button-name4');
+
+
+commercialInformation.addEventListener('click', commercialIn);
 
 function commercialIn()
 {
-    commercialQuestions.classList.remove('dissapear-service')
-    accountQuestions.classList.add('dissapear-service')
-    paymentsQuestions.classList.add('dissapear-service')
-    securityQuestions.classList.add('dissapear-service')
+    commercialQuestions.classList.remove('dissapear-service');
+    accountQuestions.classList.add('dissapear-service');
+    paymentsQuestions.classList.add('dissapear-service');
+    securityQuestions.classList.add('dissapear-service');
+
+    serviceButton1Blue.classList.remove('dissapear-service');
+    serviceButton2Gray.classList.remove('dissapear-service');
+    serviceButton3Gray.classList.remove('dissapear-service');
+    serviceButton4Gray.classList.remove('dissapear-service');
+
+    serviceButton1Gray.classList.add('dissapear-service');
+    serviceButton2Blue.classList.add('dissapear-service')
+    serviceButton3Blue.classList.add('dissapear-service');
+    serviceButton4Blue.classList.add('dissapear-service');
+
+    serviceButtonName1.classList.add('service-help-button-name-blue');
+    serviceButtonName1.classList.remove('service-help-button-name-gray');
+    serviceButtonName2.classList.remove('service-help-button-name-blue');
+    serviceButtonName2.classList.add('service-help-button-name-gray');
+    serviceButtonName3.classList.add('service-help-button-name-gray');
+    serviceButtonName3.classList.remove('service-help-button-name-blue');
+    serviceButtonName4.classList.remove('service-help-button-name-blue');
+    serviceButtonName4.classList.add('service-help-button-name-gray');
 }
 
 myAccount.addEventListener('click', accountButton)
@@ -67,6 +101,25 @@ function accountButton()
     commercialQuestions.classList.add('dissapear-service')
     paymentsQuestions.classList.add('dissapear-service')
     securityQuestions.classList.add('dissapear-service')
+
+    serviceButton1Gray.classList.remove('dissapear-service');
+    serviceButton2Gray.classList.add('dissapear-service');
+    serviceButton3Gray.classList.remove('dissapear-service');
+    serviceButton4Gray.classList.remove('dissapear-service');
+
+    serviceButton1Blue.classList.add('dissapear-service');
+    serviceButton2Blue.classList.remove('dissapear-service')
+    serviceButton3Blue.classList.add('dissapear-service');
+    serviceButton4Blue.classList.add('dissapear-service');
+
+    serviceButtonName1.classList.remove('service-help-button-name-blue');
+    serviceButtonName1.classList.add('service-help-button-name-gray');
+    serviceButtonName2.classList.add('service-help-button-name-blue');
+    serviceButtonName2.classList.remove('service-help-button-name-gray');
+    serviceButtonName3.classList.add('service-help-button-name-gray');
+    serviceButtonName3.classList.remove('service-help-button-name-blue');
+    serviceButtonName4.classList.remove('service-help-button-name-blue');
+    serviceButtonName4.classList.add('service-help-button-name-gray');
 }
 
 payments.addEventListener('click', paymentsButton);
@@ -78,6 +131,25 @@ function paymentsButton()
     accountQuestions.classList.add('dissapear-service');
     securityQuestions.classList.add('dissapear-service');
 
+    serviceButton1Gray.classList.remove('dissapear-service');
+    serviceButton2Gray.classList.remove('dissapear-service');
+    serviceButton3Gray.classList.add('dissapear-service');
+    serviceButton4Gray.classList.remove('dissapear-service');
+
+    serviceButton1Blue.classList.add('dissapear-service');
+    serviceButton2Blue.classList.add('dissapear-service')
+    serviceButton3Blue.classList.remove('dissapear-service');
+    serviceButton4Blue.classList.add('dissapear-service');
+
+    serviceButtonName1.classList.remove('service-help-button-name-blue');
+    serviceButtonName1.classList.add('service-help-button-name-gray');
+    serviceButtonName2.classList.remove('service-help-button-name-blue');
+    serviceButtonName2.classList.add('service-help-button-name-gray');
+    serviceButtonName3.classList.remove('service-help-button-name-gray');
+    serviceButtonName3.classList.add('service-help-button-name-blue');
+    serviceButtonName4.classList.remove('service-help-button-name-blue');
+    serviceButtonName4.classList.add('service-help-button-name-gray');
+
 }
 
 
@@ -85,10 +157,29 @@ security.addEventListener('click', securitybutton)
 
 function securitybutton()
 {
-    securityQuestions.classList.remove('dissapear-service')
-    commercialQuestions.classList.add('dissapear-service')
-    accountQuestions.classList.add('dissapear-service')
-    paymentsQuestions.classList.add('dissapear-service')
+    securityQuestions.classList.remove('dissapear-service');
+    commercialQuestions.classList.add('dissapear-service');
+    accountQuestions.classList.add('dissapear-service');
+    paymentsQuestions.classList.add('dissapear-service');
+
+    serviceButton1Gray.classList.remove('dissapear-service');
+    serviceButton2Gray.classList.remove('dissapear-service');
+    serviceButton3Gray.classList.remove('dissapear-service');
+    serviceButton4Gray.classList.add('dissapear-service');
+
+    serviceButton1Blue.classList.add('dissapear-service');
+    serviceButton2Blue.classList.add('dissapear-service')
+    serviceButton3Blue.classList.add('dissapear-service');
+    serviceButton4Blue.classList.remove('dissapear-service');
+
+    serviceButtonName1.classList.remove('service-help-button-name-blue');
+    serviceButtonName1.classList.add('service-help-button-name-gray');
+    serviceButtonName2.classList.remove('service-help-button-name-blue');
+    serviceButtonName2.classList.add('service-help-button-name-gray');
+    serviceButtonName3.classList.add('service-help-button-name-gray');
+    serviceButtonName3.classList.remove('service-help-button-name-blue');
+    serviceButtonName4.classList.add('service-help-button-name-blue');
+    serviceButtonName4.classList.remove('service-help-button-name-gray');
 }
 
 
